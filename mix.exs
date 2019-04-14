1a4802f2aa19f0c12c3440e404e75a7a8ffa5e7a
Ulisses Almeida <anizark@gmail.com>
2019-04-14 18:54:22 +0300

    Add dialyzer check.

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule CPF.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.20.1", only: [:dev], runtime: false}
+      {:ex_doc, "~> 0.20.1", only: [:dev], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
