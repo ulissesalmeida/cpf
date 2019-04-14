7441ba4ffc9da4151e2370f0f08f383905f50f70
Ulisses Almeida <anizark@gmail.com>
2019-04-14 18:42:17 +0300

    Add hex as development lib

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule CPF.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.20.1"}
+      {:ex_doc, "~> 0.20.1", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
