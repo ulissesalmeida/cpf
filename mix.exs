0e5d956add80593e826b9795cbd4d0df46d55476
Ulisses Almeida <anizark@gmail.com>
2019-06-16 15:33:43 +0300

    Update dev/test dependencies

--- mix.exs
+++ mix.exs
@@ -25,9 +25,9 @@ defmodule CPF.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.20.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.20", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
